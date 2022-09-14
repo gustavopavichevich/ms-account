200fec8a4117c27fda368440d81e25ee2f1e6860
Gustavo Pavichevich <gustavopavichevich@gmail.com>
2022-09-14 01:53:33 -0300

    firt commit

---
+++ src/main/kotlin/com/paciente/msaccount/MsAccountApplication.kt
@@ -0,0 +1,11 @@
+package com.paciente.msaccount
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MsAccountApplication
+
+fun main(args: Array<String>) {
+	runApplication<MsAccountApplication>(*args)
+}
